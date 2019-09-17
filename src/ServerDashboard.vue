f53cd5da10b71c0104b5f0868d285b412563b4dd
enrico.massone@deltatre.it <enrico.massone@deltatre.it>
2019-09-17 07:27:14 +0200

    copied files from other project

---
+++ src/ServerDashboard.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="row">
+      <div class="col-xs-12 col-sm-6">
+          <my-server-list></my-server-list>
+      </div>
+      <div class="col-xs-12 col-sm-6">
+          <my-server-details></my-server-details>
+      </div>
+  </div>
+</template>
+
+<script>
+  import ServerDetails from "./ServerDetails.vue";
+  import ServerList from "./ServerList.vue";
+
+  export default {
+    components: {
+      "my-server-details": ServerDetails,
+      "my-server-list": ServerList
+    }
+  };
+</script>
+
+<style>
+</style>
---
+++ src/ServerList.vue
@@ -0,0 +1,21 @@
+<template>
+  <ul class="list-group">
+      <my-server 
+        v-for="index in 5" 
+        v-bind:key="index"
+        v-bind:index="index">
+      </my-server>
+  </ul>  
+</template>
+
+<script>
+  import Server from "./Server.vue";
+
+  export default {
+    components: {
+      "my-server": Server
+    }
+  };
+</script>
+
+<style></style>
